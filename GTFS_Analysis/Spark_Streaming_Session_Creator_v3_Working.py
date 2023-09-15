fd9023504e0540eeecb4644533d474c60ba29668
Nate B <nbasch@verizon.net>
2023-09-15 16:11:25 +0800

    Add files via upload

--- GTFS_Analysis/Spark_Streaming_Session_Creator_v3_Working.py
+++ GTFS_Analysis/Spark_Streaming_Session_Creator_v3_Working.py
@@ -1,3 +1,4 @@
+from google.transit import gtfs_realtime_pb2
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
@@ -5,6 +6,10 @@ from sedona.spark import *
 from shapely import wkt
 import geopandas as gpd
 import pandas as pd
+import os
+
+#Set Pyspark_python module
+os.environ["PYSPARK_PYTHON"] = "/home/nate/virtual_envs/sedona_env/bin/python3"
 
 #Add wkt csvs as geopandas geodataframes
 chinatown_df = pd.read_csv(r'/home/nate/Documents/Python_Projects/GTFS_Analysis/Geofences/chinatown_wkt.csv')
@@ -40,46 +45,65 @@ stream_df = spark \
   .load()
 stream_df.printSchema()
 
-#Cast json value as string
-stream_df = stream_df.withColumn("value", stream_df["value"].cast("STRING"))
-
-#Define JSON schema for deserialization
-json_schema = StructType([
+# Define protobuf schema
+protobuf_schema = StructType([
+    StructField("id", StringType()),
     StructField("vehicle", StructType([
-        StructField("id", StringType()),
-        StructField("label", StringType())
-    ])),
-    StructField("position", StructType([
-        StructField("latitude", DoubleType()),
-        StructField("longitude", DoubleType()),
-        StructField("bearing", IntegerType())
-    ])),
-    StructField("current_stop_sequence", IntegerType()),
-    StructField("stop_id", StringType()),
-    StructField("current_status", StringType()),
-    StructField("timestamp", LongType()),
-    StructField("occupancy_status", StringType(), True),  # Nullable
-    StructField("occupancy_percentage", IntegerType(), True)  # Nullable
+        StructField("trip", StructType([
+            StructField("trip_id", StringType()),
+            StructField("route_id", StringType()),
+            StructField("direction_id", IntegerType()),
+            StructField("start_time", StringType()),
+            StructField("start_date", StringType()),
+            StructField("schedule_relationship", StringType())
+        ])),
+        StructField("vehicle", StructType([
+            StructField("id", StringType()),
+            StructField("label", StringType())
+        ])),
+        StructField("position", StructType([
+            StructField("latitude", DoubleType()),
+            StructField("longitude", DoubleType()),
+            StructField("bearing", IntegerType())
+        ])),
+        StructField("current_stop_sequence", IntegerType()),
+        StructField("stop_id", StringType()),
+        StructField("current_status", StringType()),
+        StructField("timestamp", TimestampType()),
+        StructField("occupancy_status", StringType(), True),  # Nullable
+        StructField("occupancy_percentage", IntegerType(), True)  # Nullable
+    ]))
 ])
 
-#Deserialize the JSON 
-stream_df = stream_df.withColumn("value", from_json("value", json_schema))
+# Define a user-defined function (UDF) to deserialize protobuf data
+def deserialize_protobuf(value):
+    feed = gtfs_realtime_pb2.FeedMessage()
+    value_bytes = bytes(value)
+    feed.ParseFromString(value_bytes)
+    return feed
+
+# Register the UDF with Spark
+deserialize_protobuf_udf = udf(deserialize_protobuf, protobuf_schema)
+
+#Deserialize the protobuf
+stream_df = stream_df \
+    .withColumn("value_struct", deserialize_protobuf_udf("value"))
 stream_df.printSchema()
 
 #Grab specific fields from schema
-stream_df = stream_df.select("value.vehicle", "value.position", "value.timestamp")
+parsed_df = stream_df.select("key", "value_struct.vehicle.vehicle", "value_struct.vehicle.position", "value_struct.vehicle.timestamp")
 
 #Parse out lat and long information
 latitude_expr = concat(lit(""), col("position.latitude")).alias("latitude")
 longitude_expr = concat(lit(""), col("position.longitude")).alias("longitude")
 
 # Create new columns with lat and long information
-parsed_df = stream_df.withColumn("latitude", latitude_expr).withColumn("longitude", longitude_expr)
+parsed_df = parsed_df.withColumn("latitude", latitude_expr).withColumn("longitude", longitude_expr)
 parsed_df.printSchema()
 parsed_df.createOrReplaceTempView("parsed_json")
 
 #Convert the geometry to wkt and create spatial dataframe
-spatial_df = sedona.sql("SELECT timestamp, st_point(longitude, latitude) AS geometry FROM parsed_json")
+spatial_df = sedona.sql("SELECT key, timestamp, st_point(longitude, latitude) AS geometry FROM parsed_json")
 
 #Start the streaming query to process data in realtime
 query = spatial_df.writeStream \
